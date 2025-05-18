c9c8be7dc7eb131fbb1a0d7112f79469fdfc4487
CuongUITSEC <phammcuongg2005@gmail.com>
2025-05-18 22:23:57 +0700

    add bai 2

---
+++ Lab3/Lab3/Bai2.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Lab3
+{
+    partial class Bai2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listViewCommand = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(554, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Listen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listViewCommand
+            // 
+            this.listViewCommand.HideSelection = false;
+            this.listViewCommand.Location = new System.Drawing.Point(76, 77);
+            this.listViewCommand.Name = "listViewCommand";
+            this.listViewCommand.Size = new System.Drawing.Size(437, 319);
+            this.listViewCommand.TabIndex = 1;
+            this.listViewCommand.UseCompatibleStateImageBehavior = false;
+            this.listViewCommand.View = System.Windows.Forms.View.List;
+            // 
+            // Bai2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(695, 450);
+            this.Controls.Add(this.listViewCommand);
+            this.Controls.Add(this.button1);
+            this.Name = "Bai2";
+            this.Text = "Bai2";
+            this.Load += new System.EventHandler(this.Bai2_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView listViewCommand;
+    }
+}
---
+++ Lab3/Lab3/Bai2.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab3
+{
+    public partial class Bai2 : Form
+    {
+        public Bai2()
+        {
+            InitializeComponent();
+        }
+
+        private void Bai2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CheckForIllegalCrossThreadCalls = false;
+            Thread serverThread = new Thread(new ThreadStart(StartUnsafeThread));
+            serverThread.Start();
+        }
+
+
+
+        void StartUnsafeThread()
+        {
+            byte[] recv = new byte[1024];
+            int bytesReceived;
+
+            Socket listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint ippeServer = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8080);
+            listenerSocket.Bind(ippeServer);
+            listenerSocket.Listen(1);
+
+            Socket clientSocket = listenerSocket.Accept();
+
+            listViewCommand.Invoke(new Action(() =>
+            {
+                listViewCommand.Items.Add(new ListViewItem("New client connected!"));
+            }));
+
+            try
+            {
+                while (clientSocket.Connected)
+                {
+                    string text = "";
+                    do
+                    {
+                        bytesReceived = clientSocket.Receive(recv);
+                        if (bytesReceived == 0)
+                        {
+                            // Client ngắt kết nối
+                            break;
+                        }
+                        text += Encoding.ASCII.GetString(recv, 0, bytesReceived);
+                    }
+                    while (!text.EndsWith("\n"));
+
+                    if (text.Length > 0)
+                    {
+                        string finalText = text.Trim();
+                        listViewCommand.Invoke(new Action(() =>
+                        {
+                            listViewCommand.Items.Add(new ListViewItem(finalText));
+                        }));
+                    }
+
+                }
+            }
+            catch (SocketException ex)
+            {
+                listViewCommand.Invoke(new Action(() =>
+                {
+                    listViewCommand.Items.Add(new ListViewItem("SocketException: " + ex.Message));
+                }));
+            }
+            finally
+            {
+                clientSocket.Close();
+                listenerSocket.Close();
+            }
+        }
+
+
+    }
+}
